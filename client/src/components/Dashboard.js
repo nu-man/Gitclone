c9b87c90f0580a550254175ddc4689766a2bd46a
nu-man <mohdnuman198@gmail.com>
2024-07-18 02:13:23 +0530

    Task data fetched

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,16 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Loading from "./Loading";
 
 function Dashboard({ loading, showloading }) {
   let navigate = useNavigate();
+  const [taskData, setTaskData] = useState({
+    taskname: "",
+    deadline: "",
+    notificatonType: "",
+    agree: "",
+  });
   const logout = () => {
     localStorage.removeItem("token");
     navigate("/login");
@@ -30,6 +36,18 @@ function Dashboard({ loading, showloading }) {
     // eslint-disable-next-line
   }, []);
 
+  const Onchnagehandler = (e) => {
+    setTaskData({
+      ...taskData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    console.log(taskData);
+  };
+
   return (
     <>
       <div
@@ -46,7 +64,7 @@ function Dashboard({ loading, showloading }) {
         <div className="main">
           <h2>Schedule New Tasks</h2>
           <hr></hr>
-          <form>
+          <form onSubmit={onSubmitHandler}>
             <label htmlFor="taskname">
               <b>Task Name</b>
             </label>
@@ -55,6 +73,7 @@ function Dashboard({ loading, showloading }) {
               type="text"
               placeholder="Enter task name"
               name="taskname"
+              onChange={Onchnagehandler}
             ></input>
             <label htmlFor="deadline">
               <b>Deadline</b>
@@ -65,26 +84,31 @@ function Dashboard({ loading, showloading }) {
               type="datetime-local"
               placeholder="Enter task deadline"
               name="deadline"
+              onChange={Onchnagehandler}
             ></input>
             <br />
             <label htmlFor="notificatonType">
               <b>Notification Type</b>
             </label>
             <br />
-            <select name="notificatonType">
-              <option value="Choose your notification type"></option>
-              <option value="SMS"></option>
-              <option value="Email"></option>
-              <option value="Both"></option>
+            <select name="notificatonType" onChange={Onchnagehandler}>
+              <option
+                value="Choose your notification type"
+                onChange={Onchnagehandler}
+              ></option>
+              <option value="SMS" onChange={Onchnagehandler}></option>
+              <option value="Email" onChange={Onchnagehandler}></option>
+              <option value="Both" onChange={Onchnagehandler}></option>
             </select>
             <hr></hr>
-            <input type="checkbox"
-            name="agree"></input>
+            <input type="checkbox" name="agree"></input>
             <label htmlFor="agree">
-                By clicking Schedule Job Button below, you agree to recieve emails and mesages as reminder notiications.
-
-            </label><br/>
-            <br/><br/>
+              By clicking Schedule Job Button below, you agree to recieve emails
+              and mesages as reminder notiications.
+            </label>
+            <br />
+            <br />
+            <br />
             <input type="submit" value="Schedule Job"></input>
           </form>
         </div>
